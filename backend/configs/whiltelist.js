3ee2d744bab251dd3e0b8b3536aed463926a788b
Steven-lei <leizhengwen@126.com>
2026-03-23 23:53:49 +1300

    white list

--- backend/configs/whiltelist.js
+++ backend/configs/whiltelist.js
@@ -1,7 +1,7 @@
 const whitelist = [
   "http://localhost:5173", //for debug
   "http://127.0.0.1:5173", //for debug
-  "https://uoa-cs732-s1-2026.github.io/", //for product env
+  "https://uoa-cs732-s1-2026.github.io", //for product env
 ];
 
 export { whitelist };
